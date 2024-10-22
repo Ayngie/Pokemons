3dc3f27562161669b9e231efd377a0078eff1d16
Ayngie <angelicareutersward@gmail.com>
2024-10-22 23:52:54 +0200

    refactor: cleaned up order of colors

--- src/utils/pokemonUtils.ts
+++ src/utils/pokemonUtils.ts
@@ -1,18 +1,18 @@
 const typeColorMap: { [key: string]: string } = {
   normal: 'text-green-800',
+  bug: 'text-green-600',
   fire: 'text-red-800',
   water: 'text-blue-800',
   flying: 'text-blue-400',
+  ice: 'text-blue-300',
+  dragon: 'text-indigo-800',
+  ghost: 'text-purple-900',
+  psychic: 'text-purple-800',
   poison: 'text-purple-600',
+  fairy: 'text-pink-600',
   electric: 'text-yellow-600',
   ground: 'text-yellow-600',
-  psychic: 'text-purple-800',
-  ice: 'text-blue-300',
-  bug: 'text-green-600',
-  ghost: 'text-purple-900',
   steel: 'text-gray-400',
-  dragon: 'text-indigo-800',
-  fairy: 'text-pink-600',
 };
 
 export const getColorForType = (type?: string | undefined) => {
