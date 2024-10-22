1ef52281229d1fc091af9dbeb1283b89b0458b9d
Ayngie <angelicareutersward@gmail.com>
2024-10-22 16:24:36 +0200

    refactor: removed function (that is now a reusable separate function instead), added import

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,32 +1,11 @@
 import { IPokemon } from '../api/pokemons';
+import { getColorForType } from '../utils/pokemonUtils';
 
 interface IPokemonProps {
   pokemon: IPokemon;
 }
 
 export const Pokemon = ({ pokemon }: IPokemonProps) => {
-  const typeColorMap: { [key: string]: string } = {
-    normal: 'text-green-800',
-    fire: 'text-red-800',
-    water: 'text-blue-800',
-    flying: 'text-blue-400',
-    fighting: 'text-gray-800',
-    poison: 'text-purple-600',
-    electric: 'text-yellow-600',
-    ground: 'text-yellow-600',
-    rock: 'text-gray-800',
-    psychic: 'text-purple-800',
-    ice: 'text-blue-300',
-    bug: 'text-green-600',
-    ghost: 'text-purple-900',
-    steel: 'text-gray-400',
-    dragon: 'text-indigo-800',
-    fairy: 'text-pink-600',
-  };
-
-  const getColorForType = (type: string) => {
-    return typeColorMap[type] || 'text-gray-800';
-  };
   return (
     <div className="my-10 shadow-lg ">
       <h2 className="text-xl">
